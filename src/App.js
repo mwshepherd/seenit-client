579ff2833cf59ecd761a483ed66c9fa84cf7169a
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-09 09:30:54 +1000

    added leaflet map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { icon } from 'leaflet';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Map center={[0, 0]} zoom={2}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+    </Map>
+  );
 }
 
 export default App;
