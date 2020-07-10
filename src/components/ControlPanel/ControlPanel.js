03de08b0f5cc7d0dd9c9068dc2e3ba0403abeeaa
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-10 20:12:19 +1000

    refined filtered search

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -4,6 +4,10 @@ import Autocomplete from './Autocomplete';
 import './ControlPanel.scss';
 
 class ControlPanel extends Component {
+  constructor() {
+    super();
+    this.match = null;
+  }
   state = {
     query: null,
   };
@@ -33,11 +37,20 @@ class ControlPanel extends Component {
 
   renderUsersCountries = () => {
     return this.props.countries.map((country) => {
-      let match = this.props.totalCountries.find(({ name }) => name === country.name);
-      console.log(match);
+      if (this.props.totalCountries.length > 1) {
+        this.match = this.props.totalCountries.find(({ name }) => name === country.name);
+      }
+
       return (
         <div key={country.id} className="user-countries__country">
-          <div className="user-countries__flag">{/* <img src={match.flag} /> */}</div>
+          {this.match && (
+            <>
+              <div className="user-countries__flag">
+                <img src={this.match.flag} />
+              </div>
+              {/* <div className="population">{this.match.population}</div> */}
+            </>
+          )}
           <div className="user-countries__country-name">{country.name}</div>
           <button className="user-countries__country-delete" onClick={() => this.props.deleteCountry(country.id)}>
             delete
@@ -52,14 +65,6 @@ class ControlPanel extends Component {
     console.log(this.props);
     return (
       <div className="control-panel">
-        <div className="form-container">
-          <h3>Search for country</h3>
-          <form onSubmit={this.handleSubmit}>
-            {/* <input type="text" placeholder="Country" onChange={this.handleOnChange} /> */}
-            <Autocomplete suggestions={this.props.predictiveCountries} handleOnChange={this.handleOnChange} handleStateChange={this.handleStateChange} />
-          </form>
-        </div>
-
         <div className="user-countries">
           <h3>Countries you've seent</h3>
           {this.props.countries && this.renderUsersCountries()}
